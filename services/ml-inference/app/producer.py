aea517f52a8cfed147ad8e31b90eca8d21c8978f
Jaiharishan <jaiharishanav@gmail.com>
2026-01-27 13:34:24 -0500

    End to End Services Working Successfully

--- services/ml-inference/app/producer.py
+++ services/ml-inference/app/producer.py
@@ -2,13 +2,32 @@ import json
 from kafka import KafkaProducer
 from app.config import KAFKA_BROKERS
 
-producer = KafkaProducer(
-    bootstrap_servers=KAFKA_BROKERS,
-    value_serializer=lambda v: json.dumps(v).encode("utf-8"),
-    key_serializer=lambda k: k.encode("utf-8")
-)
+_producer = None
+
+# producer = KafkaProducer(
+#     bootstrap_servers=KAFKA_BROKERS,
+#     value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+#     key_serializer=lambda k: k.encode("utf-8")
+# )
+
+def get_producer():
+    global _producer
+    if _producer is None:
+        _producer = KafkaProducer(
+            bootstrap_servers=KAFKA_BROKERS,
+            value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+            key_serializer=lambda k: k.encode("utf-8"),
+            retries=5
+        )
+    return _producer
 
 
 def publish(topic: str, key: str, event: dict):
+    producer = get_producer()
     producer.send(topic, key=key, value=event)
     producer.flush()
+
+
+
+
+
